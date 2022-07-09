e2f5c0268d453b3b6e1af0c77f8b521cd7d3b0bf
staryasar07@gmail.com <staryasar07@gmail.com>
2022-07-10 00:47:30 +0530

    Add project files.

---
+++ Configuration/EntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace DataVerse_MVC.Configuration
+{
+    public class EntityConfiguration
+    {
+        public const string SectionName = "EntityConfiguration";
+        public string Resource { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+        public string Authority { get; set; }
+        public string EntityInternalName { get; set; }
+        public string[] Columns { get; set; }
+    }
+}
---
+++ Controllers/ApproveRejectController.cs
@@ -0,0 +1,52 @@
+﻿using DataVerseLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+
+namespace DataVerse_MVC.Controllers
+{
+    public class ApproveRejectController : Controller
+    {
+
+        private IOptions<Configuration.EntityConfiguration> _entityConfiguration;
+
+        public ApproveRejectController(IOptions<Configuration.EntityConfiguration> entityConfiguration)
+        {
+            this._entityConfiguration = entityConfiguration;
+        }
+
+        // GET: ApproveRejectController
+        public ActionResult Index(string recordId)
+        {
+            //c7f8bf48-c1fd-ec11-82e6-000d3af256af  
+            if (!string.IsNullOrEmpty(recordId))
+            {
+                var accessToken = Crm.ConnectWithOAuth(_entityConfiguration.Value.Resource, _entityConfiguration.Value.ClientId,
+                                                       _entityConfiguration.Value.ClientSecret, _entityConfiguration.Value.Authority);
+
+                Crm.CallOut(_entityConfiguration.Value.Resource, recordId, accessToken,_entityConfiguration.Value.EntityInternalName,
+                            _entityConfiguration.Value.Columns);
+
+                return View();
+            }
+            ViewBag.name = "test name";
+            ViewBag.age = 211;
+            ViewBag.status = "test status";
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
